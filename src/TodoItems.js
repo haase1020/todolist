370b40527031e173a2004b76aa0f3b68336a9e98
Mandi <haase1020@gmail.com>
2021-02-22 06:26:36 -0500

    fork and try out project

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,35 +1,36 @@
 ﻿import React, { Component } from "react";
 
 class TodoItems extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.createTasks = this.createTasks.bind(this);
+  }
 
-        this.createTasks = this.createTasks.bind(this);
-    }
+  delete(key) {
+    this.props.delete(key);
+  }
 
-    delete(key) {
-        this.props.delete(key);
-    }
+  createTasks(item) {
+    // conditionally rendering where we only render the delete buttpn if the allow delete button is set to true
+    return (
+      <li key={item.key}>
+        {item.text}
+        {this.props.allowDelete && (
+          <button className="delete" onClick={() => this.delete(item.key)}>
+            x
+          </button>
+        )}
+      </li>
+    );
+  }
 
-    createTasks(item) {
-        // conditionally rendering where we only render the delete buttpn if the allow delete button is set to true
-        return <li
-            key={item.key}>{item.text}
-            {this.props.allowDelete && <button onClick={() => this.delete(item.key)}>x</button>}
-            </li>
-    }
+  render() {
+    var todoEntries = this.props.entries;
+    var listItems = todoEntries.map(this.createTasks);
 
-    render() {
-        var todoEntries = this.props.entries;
-        var listItems = todoEntries.map(this.createTasks);
-
-        return (
-            <ul className="theList">
-                {listItems}
-            </ul>
-        );
-    }
-};
+    return <ul className="theList">{listItems}</ul>;
+  }
+}
 
 export default TodoItems;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,93 +1,101 @@
 ﻿import React, { Component } from "react";
-import { SplitTreatments, withSplitFactory } from "@splitsoftware/splitio-react"
+import {
+  SplitTreatments,
+  withSplitFactory,
+} from "@splitsoftware/splitio-react";
 import TodoItems from "./TodoItems";
 import "./TodoList.css";
 
 class TodoList extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            items: []
+    this.state = {
+      items: [],
+    };
+    this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+
+  addItem(e) {
+    console.log(process.env);
+    if (this._inputElement.value !== "") {
+      var newItem = {
+        text: this._inputElement.value,
+        key: Date.now(),
+      };
+
+      this.setState((prevState) => {
+        return {
+          items: prevState.items.concat(newItem),
         };
-        this.addItem = this.addItem.bind(this);
-        this.deleteItem = this.deleteItem.bind(this);
+      });
+
+      this._inputElement.value = "";
     }
 
-    addItem(e) {
-        if (this._inputElement.value !== "") {
-            var newItem = {
-                text: this._inputElement.value,
-                key: Date.now()
-            };
+    console.log(this.state.items);
 
-            this.setState((prevState) => {
-                return {
-                    items: prevState.items.concat(newItem)
-                };
-            });
+    e.preventDefault();
+  }
 
-            this._inputElement.value = "";
-        }
+  deleteItem(key) {
+    var filteredItems = this.state.items.filter(function (item) {
+      return item.key !== key;
+    });
 
-        console.log(this.state.items);
+    this.setState({
+      items: filteredItems,
+    });
+  }
 
-        e.preventDefault();
-    }
+  //React render function to render the todo list
+  render() {
+    return (
+      //Split Treatments is a React component that performs feature evalutation
+      //Does a call to client.getTreatmentswithConfig(names)
+      //You pass in the name of the feature flag
+      //This returns a treatment and a config
+      <SplitTreatments names={["talia_todolist_delete"]}>
+        {({ treatments }) => {
+          return this.renderContent(treatments["talia_todolist_delete"]);
+        }}
+      </SplitTreatments>
+    );
+  }
 
-    deleteItem(key) {
-        var filteredItems = this.state.items.filter(function (item) {
-            return (item.key !== key);
-        });
-
-        this.setState({
-            items: filteredItems
-        });
-    }
-
-    //React render function to render the todo list
-    render() {
-        return (
-            //Split Treatments is a React component that performs feature evalutation
-            //Does a call to client.getTreatmentswithConfig(names)
-            //You pass in the name of the feature flag
-            //This returns a treatment and a config
-            <SplitTreatments names={['talia_todolist_delete']} >
-                {({ treatments }) => {
-                    return this.renderContent(treatments['talia_todolist_delete']) 
-                }}
-            </SplitTreatments>
-        );
-    }
-
-    renderContent(deleteTreatment) {
-        const allowDelete = deleteTreatment.treatment === 'on';
-        return (
-            <div className="todoListMain">
-                <div className="header">
-                    <form onSubmit={this.addItem}>
-                        <input ref={(a) => this._inputElement = a}
-                            placeholder="Enter Task">
-                        </input>
-                        <button type="submit">Add</button> 
-                    </form>
-                </div>
-                <TodoItems entries={this.state.items} allowDelete={allowDelete}
-                    delete={this.deleteItem} />
-            </div>
-            )
-    }
+  renderContent(deleteTreatment) {
+    const allowDelete = deleteTreatment.treatment === "on";
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input
+              ref={(a) => (this._inputElement = a)}
+              placeholder="Enter Task"
+            ></input>
+            <button type="submit">Add</button>
+          </form>
+        </div>
+        <TodoItems
+          entries={this.state.items}
+          allowDelete={allowDelete}
+          delete={this.deleteItem}
+        />
+      </div>
+    );
+  }
 }
 
 // The config that I use to configure my split instance
 // This initializes the Split Factory, which is the entry point of the library
 // From Syntax in UI
 const sdkConfig = {
-    core: {
-        authorizationKey: 's2959s3memm2hp1b03u34khu1sjl0106j6qr',
-        key: 'talia.nassi@split.io'
-    },
-    debug: true
+  core: {
+    authorizationKey: `${process.env.REACT_APP_SPLIT_AUTH_KEY}`,
+    key: "mandi@prenda.co",
+  },
+  debug: true,
 };
 
 export default withSplitFactory(sdkConfig)(TodoList);
